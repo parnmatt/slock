882e1b2b0f044be728dbdb5d364e193788e3ba59
Matthew Parnell <matt@parnmatt.co.uk>
2017-09-06 20:12:29 +0100

    Implement gruvbox colour scheme

--- config.h
+++ config.h
@@ -6,10 +6,10 @@ static const char *user  = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#282828", /* after initialisation */
+	[INPUT] =  "#458588", /* during input */
+	[FAILED] = "#fb4934", /* wrong password */
 };
 
-/* treat a cleared input like a wrong password (color) */
+/* treat a cleared input like a wrong password (colour) */
 static const int failonclear = 1;
